4a89482c20a773e61f6220dae021dbd4015192b7
hydrogenbear <hydrogenbear@poption.org>
2025-07-07 16:01:58 +0800

    first commit

---
+++ grandpas_clock.py
@@ -0,0 +1,58 @@
+from midiutil import MIDIFile
+
+# 创建一个新的MIDI文件
+midi = MIDIFile(1) # 1个轨道
+track = 0
+channel = 0
+time = 0    # 以拍为单位的起始时间
+tempo = 110 # BPM (每分钟110拍)
+volume = 100 # 音量 (0-127)
+
+midi.addTempo(track, time, tempo)
+
+# 定义音符参数
+pitch_c4 = 60 # C4 的MIDI音高值 (中央C)
+duration_half_note = 2 # 二分音符持续2拍
+
+# --- 第 1 小节 ---
+# 右手 C4 (第一个二分音符)
+midi.addNote(track, channel, pitch_c4, time, duration_half_note, volume)
+time += duration_half_note
+
+# 右手 C4 (第二个二分音符)
+midi.addNote(track, channel, pitch_c4, time, duration_half_note, volume)
+time += duration_half_note # 小节结束，时间推进
+
+# --- 第 2 小节 ---
+# 左手 C4 (第一个二分音符)
+midi.addNote(track, channel, pitch_c4, time, duration_half_note, volume)
+time += duration_half_note
+
+# 左手 C4 (第二个二分音符)
+midi.addNote(track, channel, pitch_c4, time, duration_half_note, volume)
+time += duration_half_note # 小节结束，时间推进
+
+# --- 第 3 小节 (重复第 1 小节) ---
+# 右手 C4 (第一个二分音符)
+midi.addNote(track, channel, pitch_c4, time, duration_half_note, volume)
+time += duration_half_note
+
+# 右手 C4 (第二个二分音符)
+midi.addNote(track, channel, pitch_c4, time, duration_half_note, volume)
+time += duration_half_note # 小节结束，时间推进
+
+# --- 第 4 小节 (重复第 2 小节) ---
+# 左手 C4 (第一个二分音符)
+midi.addNote(track, channel, pitch_c4, time, duration_half_note, volume)
+time += duration_half_note
+
+# 左手 C4 (第二个二分音符)
+midi.addNote(track, channel, pitch_c4, time, duration_half_note, volume)
+# time += duration_half_note # 最后一个音符，不需要再推进时间
+
+# 将MIDI文件写入到磁盘
+with open("out/grandpas_clock_4_measures.mid", "wb") as output_file:
+    midi.writeFile(output_file)
+
+print("MIDI文件 'grandpas_clock_4_measures.mid' 已成功生成！")
+print("它包含四个小节，后两个小节重复前两个。")
